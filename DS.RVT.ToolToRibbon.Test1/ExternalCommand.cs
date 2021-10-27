83b6db89e6a4f1dacb1c9ce9e8cfab25dd1ad1cd
Danil <leonardo147wot@gmail.com>
2021-10-27 18:45:05 +0300

    Intersected elements parsing

--- DS.RVT.ToolToRibbon.Test1/ExternalCommand.cs
+++ DS.RVT.ToolToRibbon.Test1/ExternalCommand.cs
@@ -8,11 +8,21 @@ namespace DS.RVT.ToolToRibbon.Test1
     [Transaction(TransactionMode.Manual)]
     public class ExternalCommand : IExternalCommand
     {
-        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
            ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Revit", "Hello World");
+            UIApplication uiapp = commandData.Application;
+
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            Intersection intersection = new Intersection();
+            intersection.FindIntersections(uidoc, doc);
+
+
             return Autodesk.Revit.UI.Result.Succeeded;
         }
+      
+
     }
 }
---
+++ DS.RVT.ToolToRibbon.Test1/Intersection.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DS.RVT.ToolToRibbon.Test1
+{
+    class Intersection
+    {
+
+        public void FindIntersections(UIDocument uidoc, Document doc)
+        {
+
+            // Find intersections between elements and a selected element
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element, "Select element that will be checked for intersection with all elements");
+            Element element = doc.GetElement(reference);
+            GeometryElement geomElement = element.get_Geometry(new Options());
+
+            Solid solid = null;
+            foreach (GeometryObject geomObj in geomElement)
+            {
+                solid = geomObj as Solid;
+                if (solid != null) break; 
+            }
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            collector.OfClass(typeof(Pipe));
+
+            ElementIntersectsSolidFilter intersectionFilter = new ElementIntersectsSolidFilter(solid);
+            collector.WherePasses(intersectionFilter); // Apply intersection filter to find matches
+
+            IList<Element> elements = collector.ToElements();
+
+            foreach (Element e in elements)
+            {
+
+                TaskDialog.Show("Revit", collector.Count() + 
+                    " family instances intersect with the selected element (" + e.Category.Name + " id:" + e.Id.ToString() + ")");
+
+            }
+
+
+
+        }
+    }
+}
