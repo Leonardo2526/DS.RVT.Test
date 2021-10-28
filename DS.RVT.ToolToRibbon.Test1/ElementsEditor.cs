4bd767cc12231e6bf414784cea0a44ae1c080edd
Danil <leonardo147wot@gmail.com>
2021-10-28 22:09:10 +0300

    movement error fixed

--- DS.RVT.ToolToRibbon.Test1/ElementsEditor.cs
+++ DS.RVT.ToolToRibbon.Test1/ElementsEditor.cs
@@ -25,32 +25,39 @@ namespace DS.RVT.ToolToRibbon.Test1
             LocationCurve lc = element.Location as LocationCurve;
             Curve c = lc.Curve;
             c.GetEndPoint(0);
-            c.GetEndPoint(1);
+            c.GetEndPoint(1); 
 
             XYZ startPoint = c.GetEndPoint(0);
             XYZ endPoint = c.GetEndPoint(1);
-            XYZ centerPoint = new XYZ(Math.Abs(startPoint.X - endPoint.X) / 2,
-                Math.Abs(startPoint.Y - endPoint.Y)/2, 
-                Math.Abs(startPoint.Z - endPoint.Z)/2);
+            XYZ centerPoint = new XYZ((startPoint.X + endPoint.X) / 2,
+                (startPoint.Y + endPoint.Y)/2, 
+                (startPoint.Z + endPoint.Z)/2);
 
             RevitElements revitElements = new RevitElements(Uidoc, Doc);
             revitElements.CreateModelLine(startPoint, endPoint);
 
+            // Set offset of element
+            double offsetX = 1000;
+            double offsetXF = UnitUtils.Convert(offsetX / 1000,
+                                   DisplayUnitType.DUT_METERS,
+                                   DisplayUnitType.DUT_DECIMAL_FEET);
 
+            XYZ newPlace = new XYZ(centerPoint.X + offsetXF, centerPoint.Y, centerPoint.Z);
+            XYZ newVector = new XYZ(offsetXF, 0, 0);
+            revitElements.CreateModelLine(centerPoint, newPlace);
+
+            MoveElementTransaction(element, newVector);
+
+        }
+
+        public void MoveElementTransaction(Element element, XYZ newVector)
+        {
             using (Transaction transNew = new Transaction(Doc, "MoveElement"))
             {
                 try
                 {
                     transNew.Start();
-
-                    // Move the column to new location.
-                    double offsetX = 1000;
-                    double offsetXF = UnitUtils.Convert(offsetX / 1000,
-                                           DisplayUnitType.DUT_METERS,
-                                           DisplayUnitType.DUT_DECIMAL_FEET);
-
-                    XYZ newPlace = new XYZ(centerPoint.X + offsetXF, centerPoint.Y, centerPoint.Z);
-                    ElementTransformUtils.MoveElement(Doc, element.Id, newPlace);
+                    ElementTransformUtils.MoveElement(Doc, element.Id, newVector);
                 }
 
                 catch (Exception e)
@@ -61,8 +68,6 @@ namespace DS.RVT.ToolToRibbon.Test1
 
                 transNew.Commit();
             }
-
-
         }
 
     }
