0777f59c5d857d831cc4bd6f15664d3d8549711e
Danil <leonardo147wot@gmail.com>
2021-10-28 13:21:50 +0300

    Try to make offset for BB

--- DS.RVT.ToolToRibbon.Test1/Intersection.cs
+++ DS.RVT.ToolToRibbon.Test1/Intersection.cs
@@ -19,43 +19,78 @@ namespace DS.RVT.ToolToRibbon.Test1
             Element element = doc.GetElement(reference);
             GeometryElement geomElement = element.get_Geometry(new Options());
 
+            Solid solid = null;
+            foreach (GeometryObject geomObj in geomElement)
+            {
+                solid = geomObj as Solid;
+                if (solid != null) break;
+            }
+
+            BoundingBoxXYZ bb = solid.GetBoundingBox();
+
+            // Find the bounding box from the selected 
+            // object and convert to outline.
+            //BoundingBoxXYZ bb = element.get_BoundingBox(doc.ActiveView);
+
+            Transform trf = bb.Transform;
+
+            XYZ maxInModelCoords = trf.OfPoint(bb.Max);
+            XYZ minInModelCoords = trf.OfPoint(bb.Min);
+
+            double offset = 100;
+
+            XYZ solidMax = new XYZ(maxInModelCoords.X + offset, maxInModelCoords.Y + offset, maxInModelCoords.Z + offset);
+            XYZ solidMin = new XYZ(minInModelCoords.X - offset, minInModelCoords.Y - offset, minInModelCoords.Z - offset);
+
+            XYZ solidMax1 = new XYZ(bb.Max.X, bb.Max.Y, bb.Max.Z);
+            XYZ solidMin1 = new XYZ(bb.Min.X, bb.Min.Y, bb.Min.Z);
+
+            Outline outline = new Outline(solidMin, solidMax);
+            //Outline outline = new Outline(bb.Min, bb.Max);
+
+            // Create a BoundingBoxIntersectsFilter to 
+            // find everything intersecting the bounding 
+            // box of the selected element.
+
+            BoundingBoxIntersectsFilter bbfilter = new BoundingBoxIntersectsFilter(outline);
+
+            // Use a view to construct the filter so we 
+            // get only visible elements. For example, 
+            // the analytical model will be found otherwise.
+            FilteredElementCollector collector = new FilteredElementCollector(doc, doc.ActiveView.Id);
+
+            //ElementIntersectsSolidFilter intersectionFilter = new ElementIntersectsSolidFilter(solid);
+
+            collector.OfClass(typeof(Pipe));
+
+            // Apply intersection filter to find matches
+            collector.WherePasses(bbfilter); 
+
+
             // Get all element ids which are current selected by users
             ICollection<ElementId> selectedIds = new List<ElementId>
             {
                 element.Id
             };
 
-            Solid solid = null;
-            foreach (GeometryObject geomObj in geomElement)
-            {
-                solid = geomObj as Solid;
-                if (solid != null) break; 
-            }
-
-            FilteredElementCollector collector = new FilteredElementCollector(doc);
-
-            collector.OfClass(typeof(Pipe));
-
-            ElementIntersectsSolidFilter intersectionFilter = new ElementIntersectsSolidFilter(solid);
-            collector.WherePasses(intersectionFilter); // Apply intersection filter to find matches
-            
-
             // Use the selection to instantiate an exclusion filter
             ExclusionFilter exclusionFilter = new ExclusionFilter(selectedIds);
             collector.WherePasses(exclusionFilter);
-
+           
             IList<Element> elements = collector.ToElements();
 
+
+            string IDS = "";
+            string names = "";
             foreach (Element e in elements)
             {
-
-                TaskDialog.Show("Revit", collector.Count() + 
-                    " family instances intersect with the selected element (" + e.Category.Name + " id:" + e.Id.ToString() + ")");
+                IDS += "\n" + e.Id.ToString();
+                names += "\n"  + e.Category.Name;
 
             }
 
-
-
+            TaskDialog.Show("Revit", collector.Count() +
+                    " element intersect with the next elements (" + names + " id:" + IDS + ")");
         }
     }
 }
