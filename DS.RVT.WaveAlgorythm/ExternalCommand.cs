0193b10883bbad8a87fa08c4662d2474b9df7200
Danil <leonardo147wot@gmail.com>
2021-11-03 12:48:51 +0300

    WaveAlgorythm

---
+++ DS.RVT.WaveAlgorythm/ExternalCommand.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace DS.RVT.WaveAlgorythm
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExternalCommand : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+           ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            Autodesk.Revit.ApplicationServices.Application application = uiapp.Application;
+
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            TaskDialog.Show("Revit", "Hello Wave");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        } 
+    }
+}
