d91f8ea12be73eb5b62fe5c4cc0cc520ac3f368c
Leonardo2526 <leonardo147wot@gmail.com>
2021-12-22 14:31:49 +0300

    ribbon restored

---
+++ DS.RVT.RibbonForTest/Button.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RVT.RibbonForTest
+{
+    class Button
+    {
+        public static PushButtonData button1;
+        public static PushButtonData button2;
+        public static PushButtonData button3;
+
+        string ButtonPath;
+        string ButtonDescription;
+
+        public void AddButtons()
+        {
+            //Create button1
+            string button1Name = "DS.PipesCollisionsElliminator";
+            AddButton1(button1Name);
+
+            //Create button2
+            string button2Name = "DS.RVT.PipeTest";
+            AddButton2(button2Name);
+
+            //Create button3
+            string button3Name = "DS.RVT.WaveAlgorythm";
+            AddButton3(button3Name);
+        }
+
+        void AddButton1(string buttonName)
+        {
+            AssignProperties(buttonName);
+
+            // Create push button
+            button1 = new PushButtonData(buttonName, buttonName, ButtonPath, buttonName + ".ExternalCommand")
+            {
+                ToolTip = ButtonDescription
+            };
+        }
+
+        void AddButton2(string buttonName)
+        {
+            AssignProperties(buttonName);
+
+            // Create push button
+            button2 = new PushButtonData(buttonName, buttonName, ButtonPath, buttonName + ".ExternalCommand")
+            {
+                ToolTip = ButtonDescription
+            };
+        }
+
+
+        void AddButton3(string buttonName)
+        {
+            AssignProperties(buttonName);
+
+            // Create push button
+            button3 = new PushButtonData(buttonName, buttonName, ButtonPath, buttonName + ".ExternalCommand")
+            {
+                ToolTip = ButtonDescription
+            };
+        }
+
+        void AssignProperties(string buttonName)
+        {
+            string path = String.Format(@"%AppData%\Autodesk\Revit\Addins\2020\{0}\{0}.dll", buttonName);
+            ButtonPath = Environment.ExpandEnvironmentVariables(path);
+
+            Assembly assembly = Assembly.LoadFrom(ButtonPath);
+
+            var descriptionAttribute1 = assembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false).
+                OfType<AssemblyDescriptionAttribute>().FirstOrDefault();
+
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            ButtonDescription = descriptionAttribute1.Description + "\nProduct version: " + version;
+        }
+    }
+}
---
+++ DS.RVT.RibbonForTest/ExternalApplication.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DS.RVT.RibbonForTest
+{
+    public class ExternalApplication : IExternalApplication
+    {
+        // class instance
+        internal static ExternalApplication thisApp = null;
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            Button button = new Button();
+            button.AddButtons();
+
+            // Create a custom ribbon tab  
+            string tabName = "DS.RVT.RibbonForTest";
+            application.CreateRibbonTab(tabName); 
+
+            // Create a ribbon panel 
+            RibbonPanel m_projectPanel_1 = application.CreateRibbonPanel(tabName, "Tools");
+
+
+            // Add the button to the panel
+            // RibbonItem ribbonItem = m_projectPanel_1.AddItem(Button.button1);
+            List<RibbonItem> projectButtons = new List<RibbonItem>();
+
+            // Add the buttons to the panels
+            projectButtons.AddRange(m_projectPanel_1.AddStackedItems(Button.button1, Button.button2, Button.button3));
+
+            return Result.Succeeded;
+        }
+    }
+}
