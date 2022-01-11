0cd4a37ade79610376bddf6e787fb739af2717a5
Leonardo2526 <leonardo147wot@gmail.com>
2022-01-11 23:55:16 +0300

    PointsMarkerIterator created

---
+++ DS.RVT.ModelSpaceFragmentation/Path/PointsMarkerIterator.cs
@@ -0,0 +1,89 @@
+﻿using DS.RVT.ModelSpaceFragmentation.Points;
+using System.Collections.Generic;
+
+namespace DS.RVT.ModelSpaceFragmentation.Path
+{
+    class PointsMarkerIterator
+    {
+        static int x, y, z, d, a;
+    
+        public static StepPoint StartStepPoint;
+        public static StepPoint EndStepPoint;
+
+        public static Dictionary<StepPoint, int> Grid;
+        private static List<StepPoint> initialPriorityList;
+        private static List<StepPoint> clzPoints;
+
+        public static bool IfWaveReachedEndPoint()
+        {
+            FillData();
+            Iterate();
+
+            if (!Grid.ContainsKey(EndStepPoint))
+                return false;
+
+            return true;
+        }
+
+        static void FillData()
+        {
+         
+            StartStepPoint = new StepPoint(InputData.Ax, InputData.Ay, InputData.Az);
+            EndStepPoint = new StepPoint(InputData.Bx, InputData.By, InputData.Bz);
+
+            CLZCretor clzCreator = new CLZCretor();
+            clzPoints = clzCreator.Create(new CLZByBoders());
+
+            List<StepPoint> stepPointsList = new List<StepPoint>();
+            stepPointsList.Add(StartStepPoint);
+
+            Grid = new Dictionary<StepPoint, int>
+            {
+                { StartStepPoint, 1 }
+            };
+            Priority PriorityInstance = new Priority();
+            initialPriorityList = PriorityInstance.GetPriorities();
+        }
+
+        static void Iterate()
+        {
+            do
+            {
+                a = 0;
+                for (z = 0; z < InputData.Zcount; z++)
+                {
+                    for (y = 0; y < InputData.Ycount; y++)
+                    {
+                        for (x = 0; x < InputData.Xcount; x++)
+                        {
+                            if (!Operation())
+                                continue;
+                        }
+                    }
+                }
+            } while (!Grid.ContainsKey(EndStepPoint) && a != 0);
+        }
+
+        static bool Operation()
+        {
+            StepPoint currentPoint = new StepPoint(x, y, z);
+
+            int currentD;
+            if (!Grid.ContainsKey(currentPoint))
+                return false;
+            else
+                currentD = Grid[currentPoint];
+
+            //Create points cloud for next check from unpassible points
+            PointsCloud pointsCloud = new PointsCloud(SpaceFragmentator.UnpassablePoints);
+            List<StepPoint> unpassableByCLZPoints =
+                pointsCloud.GetStepPointByCenterPoint(PointConvertor.StepPointToXYZ(currentPoint));
+
+            PointMarker pointMarker =
+                new PointMarker(currentPoint, clzPoints, unpassableByCLZPoints, initialPriorityList, currentD);
+            pointMarker.Mark(ref Grid, ref d, ref a);
+
+            return true;
+        }
+    }
+}
--- DS.RVT.ModelSpaceFragmentation/Path/WaveAlgorythm.cs
+++ DS.RVT.ModelSpaceFragmentation/Path/WaveAlgorythm.cs
@@ -13,24 +13,11 @@ namespace DS.RVT.ModelSpaceFragmentation.Path
     {
         public List<XYZ> PathCoords { get; set; } = new List<XYZ>();
 
-        int x, y, z, d, k, a, len;
-        readonly int Ax = InputData.Ax;
-        readonly int Ay = InputData.Ay;
-        readonly int Az = InputData.Az;
-
-        readonly int Bx = InputData.Bx;
-        readonly int By = InputData.By;
-        readonly int Bz = InputData.Bz;
-
-        StepPoint StartStepPoint;
-        StepPoint EndStepPoint;
-
-        Dictionary<StepPoint, int> Grid;
-        readonly Priority PriorityInstance = new Priority();
+        int x, y, z, d, k, len;
 
         public List<XYZ> Implement()
         {
-            if (!MarkSpacePoints())
+            if (!IfPathExists())
             {
                 TaskDialog.Show("Revit", "Путь не найден!");
                 return new List<XYZ>();
@@ -39,64 +26,16 @@ namespace DS.RVT.ModelSpaceFragmentation.Path
             return PathCoords;
         }
 
-        bool MarkSpacePoints()
+        bool IfPathExists()
         {
-            StartStepPoint = new StepPoint(Ax, Ay, Az);
-            EndStepPoint = new StepPoint(Bx, By, Bz);
-
-            CLZCretor clzCreator = new CLZCretor();
-            List<StepPoint> clzPoints = clzCreator.Create(new CLZByBoders());
-
-            List<StepPoint> stepPointsList = new List<StepPoint>();
-            stepPointsList.Add(StartStepPoint);
-
-            Grid = new Dictionary<StepPoint, int>
-            {
-                { StartStepPoint, 1 }
-            };
-
-            List<StepPoint> initialPriorityList = PriorityInstance.GetPriorities();
-
-            do
-            {
-                a = 0;
-                for (z = 0; z < InputData.Zcount; z++)
-                {
-                    for (y = 0; y < InputData.Ycount; y++)
-                    {
-                        for (x = 0; x < InputData.Xcount; x++)
-                        {
-                            StepPoint currentPoint = new StepPoint(x, y, z);
-
-                            int currentD;
-                            if (!Grid.ContainsKey(currentPoint))
-                                continue;
-                            else
-                                currentD = Grid[currentPoint];
-
-                            //Create points cloud for next check from unpassible points
-                            PointsCloud pointsCloud = new PointsCloud(SpaceFragmentator.UnpassablePoints);
-                            List <StepPoint> unpassableByCLZPoints =
-                                pointsCloud.GetStepPointByCenterPoint(PointConvertor.StepPointToXYZ(currentPoint));
-
-                            PointMarker pointMarker = 
-                                new PointMarker(currentPoint, clzPoints, unpassableByCLZPoints, initialPriorityList, currentD);
-                            pointMarker.Mark(ref Grid, ref d, ref a);
-                        }
-                    }
-                }
-            } while (!Grid.ContainsKey(EndStepPoint) && a != 0);
-
-
-            if (!Grid.ContainsKey(EndStepPoint))
-                return false;
-
+            PointsMarkerIterator.IfWaveReachedEndPoint();
+           
             //List<StepPoint> items = grid.Select(d => d.Key).ToList();
             //ShowPoints(items);
 
             GetPath();
 
-            if (x == Ax && y == Ay && z == Az)
+            if (x == InputData.Ax && y == InputData.Ay && z == InputData.Az)
             {
                 return true;
             }
@@ -106,15 +45,15 @@ namespace DS.RVT.ModelSpaceFragmentation.Path
 
         void GetPath()
         {
-            Grid[StartStepPoint] = 0;
+            PointsMarkerIterator.Grid[PointsMarkerIterator.StartStepPoint] = 0;
 
             // восстановление пути
             // длина кратчайшего пути из (ax, ay) в (bx, By)
-            len = Grid[EndStepPoint];
+            len = PointsMarkerIterator.Grid[PointsMarkerIterator.EndStepPoint];
 
-            x = Bx;
-            y = By;
-            z = Bz;
+            x = InputData.Bx;
+            y = InputData.By;
+            z = InputData.Bz;
             d = len;
 
             while (d >= 0)
@@ -125,7 +64,9 @@ namespace DS.RVT.ModelSpaceFragmentation.Path
                 d--;
 
                 StepPoint currentPoint = new StepPoint(x, y, z);
-                List<StepPoint> BackWayPriorityList = PriorityInstance.GetPrioritiesByPoint(currentPoint, EndStepPoint);
+                Priority PriorityInstance = new Priority();
+                List<StepPoint> BackWayPriorityList = 
+                    PriorityInstance.GetPrioritiesByPoint(currentPoint, PointsMarkerIterator.EndStepPoint);
 
                 for (k = 0; k < 6; ++k)
                 {
@@ -135,13 +76,13 @@ namespace DS.RVT.ModelSpaceFragmentation.Path
 
                     StepPoint nextPoint = new StepPoint(ix, iy, iz);
 
-                    if (!Grid.ContainsKey(nextPoint))
+                    if (!PointsMarkerIterator.Grid.ContainsKey(nextPoint))
                         continue;
 
                     if (ix >= 0 && ix < InputData.Xcount &&
                         iy >= 0 && iy < InputData.Ycount &&
                         iz >= 0 && iz < InputData.Zcount &&
-                         Grid[nextPoint] == d)
+                         PointsMarkerIterator.Grid[nextPoint] == d)
                     {
 
                         // переходим в ячейку, которая на 1 ближе к старту
