8cd4734be11b3aeb82efcc131d6ea701f0e9bbab
Данил Сараев <leonardo147wot@gmail.com>
2022-11-01 14:51:23 +0300

    TrgEventBuilder refactored

---
+++ DS.RVT.ExtCmd.TransactionTest/ITaskEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace DS.RevitApp.TransactionTest
+{
+    /// <summary>
+    /// Interface to create task events.
+    /// </summary>
+    internal interface ITaskEvent
+    {
+        /// <summary>
+        /// Create a new task event.
+        /// </summary>
+        /// <returns></returns>
+        public Task Create();
+    }
+}
--- DS.RVT.ExtCmd.TransactionTest/ViewModel/TransactioinTestViewModel.cs
+++ DS.RVT.ExtCmd.TransactionTest/ViewModel/TransactioinTestViewModel.cs
@@ -33,75 +33,11 @@ namespace DS.RevitApp.TransactionTest.ViewModel
 
         public ICommand Commit => new RelayCommand(async c =>
         {
-            //await new TrgBuilder(_transactionWindow, _doc).Build(() => _model.CreateRevitTask());
-            await new TransactionGroupBuilder(_transactionWindow, _doc).BuildAsync(() => _model.Create(), true);
+            var taskEvent = new WindowTaskEvent(_transactionWindow, new List<Button> { _transactionWindow.RollBack });
+            await new TrgEventBuilder(_doc, taskEvent).
+            BuildAsync(() => _model.Create(), true);
 
         });
-
-        public ICommand CommitOld => new RelayCommand(async c =>
-        {
-            using (TransactionGroup trg = new TransactionGroup(_doc))
-            {
-                trg.Start();
-
-
-                Task task = Task.Run(() =>
-                {
-                    while (true)
-                    {
-                        _model.Create();
-                        //_uiDoc.RefreshActiveView();
-
-                        OnWindowAsync(_transactionWindow).Wait();
-                        //MessageBox.Show($"I was clicked at {DateTime.Now:HH:mm:ss.fffff}!\r\n");
-                        break;
-                    }
-                });
-
-                await task;
-
-                if (trg.HasStarted() && _transactionWindow.IsActive)
-                {
-                    trg.RollBack();
-                }
-                else
-                {
-                    trg.Commit();
-                }
-            }
-
-            //MessageBox.Show("Task completed!");
-        });
-        private Task ClickAsync(Button button1)
-        {
-            var tcs = new TaskCompletionSource<object>();
-            void handler(object s, EventArgs e) => tcs.TrySetResult(null);
-            button1.Click += handler;
-            return tcs.Task.ContinueWith(_ => button1.Click -= handler);
-        }
-
-        private Task OnWindowAsync(TransactionWindow window)
-        {
-            var tcs = new TaskCompletionSource<object>();
-            void handler(object s, EventArgs e) => tcs.TrySetResult(null);
-            window.Closed += handler;
-            window.RollBack.Click += handler;
-
-            return tcs.Task.ContinueWith(_ =>
-            {
-                window.Closed -= handler;
-                window.RollBack.Click -= handler;
-            });
-        }
-
-        private Task EventAsync(object obj, string eventName)
-        {
-            var eventInfo = obj.GetType().GetEvent(eventName);
-            var tcs = new TaskCompletionSource<object>();
-            EventHandler handler = delegate (object s, EventArgs e) { tcs.TrySetResult(null); };
-            eventInfo.AddEventHandler(obj, handler);
-            return tcs.Task.ContinueWith(_ => eventInfo.RemoveEventHandler(obj, handler));
-        }
 
         public ICommand RollBack => new RelayCommand(c =>
         {
