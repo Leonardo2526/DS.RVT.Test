1305af288a40b2bb6973c6cfc6da75ef6c629b74
Данил Сараев <leonardo147wot@gmail.com>
2022-11-08 12:44:50 +0300

    DS.RevitApp.SwitchSolution added

---
+++ DS.RevitApp.SwitchSolution/ViewModels/NamesCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DS.RevitApp.SwitchSolution
+{
+    public class NamesCollection : ObservableCollection<MyObject>
+    {
+        public void Add(string name)
+        {
+            this.Add(new MyObject { Name = name });
+        }
+    }
+}
