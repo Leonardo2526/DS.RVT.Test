72a66d172a15de6cc7c141c5c4dab8a0d21de91d
Данил Сараев <leonardo147wot@gmail.com>
2022-11-12 09:09:51 +0300

    RunDeleteTransaction

--- DS.RevitApp.TransactionTest/ViewModel/TransactioinTestViewModel.cs
+++ DS.RevitApp.TransactionTest/ViewModel/TransactioinTestViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using DS.ClassLib.VarUtils;
-using DS.ClassLib.VarUtils.TaskEvents;
+using DS.ClassLib.VarUtils.Events;
 using DS.RevitApp.TransactionTest.Model;
 using DS.RevitApp.TransactionTest.View;
+using DS.RevitLib.Utils.Transactions;
 using Revit.Async;
 using System;
 using System.Collections.Generic;
@@ -18,7 +20,7 @@ using System.Windows.Input;
 
 namespace DS.RevitApp.TransactionTest.ViewModel
 {
-    public class TransactioinTestViewModel : INotifyPropertyChanged, IEventHandler
+    public class TransactioinTestViewModel : INotifyPropertyChanged, IEvent<EventType>
     {
         private readonly Document _doc;
         private readonly UIDocument _uiDoc;
@@ -33,108 +35,57 @@ namespace DS.RevitApp.TransactionTest.ViewModel
             _transactionWindow = transactionWindow;
         }
 
-        public ICommand Commit => new RelayCommand(async c =>
-        {           
-
-            Debug.Print("\nCommand started");
-            Task task1 = Task.Run(async () =>
-            {
-            IWindowTaskEvent taskEvent = new HandlerTaskEvent(this);
-                await new TrgEventBuilder_1(_doc, _uiDoc, taskEvent, 0).
-                      BuildAsync(() => _model.Create(), false);
-
-            });
-            await task1;
-
-            Debug.Print("Command executed");
-        });
-
         public ICommand CommitOld => new RelayCommand(async c =>
         {
-            //new RevitAsyncTest(_doc, _uiDoc).CreateTransaction();
-
-            //var taskEvent = new WindowTaskEvent(_transactionWindow, new List<Button> { _transactionWindow.RollBack });
-            //await new TrgEventBuilder(_doc, taskEvent).
 
             Debug.Print("\nCommand started");
-
             Task task1 = Task.Run(async () =>
             {
-               await RunTransaction(1, 0, 4000);
-            });
-            Task task2 = Task.Run(async () =>
-            {
-                await RunTransaction(2, 5, 2000);
+                var taskEvent = new TaskComplition(this);
+                await new TrgEventBuilder(_doc).
+                      BuildAsync(() => _model.Create(), taskEvent, false);
+
             });
             await task1;
-            await task2;
-
-            //IWindowTaskEvent taskEvent = new HandlerTaskEvent(this);
-
-            //Debug.Print("Start Transaction");
-
-            //using (var trg = new TransactionGroup(_doc, $"1"))
-            //{
-            //    trg.Start();
-            //    await new TrgEventBuilder_1(_doc, taskEvent, 0).
-            //        BuildAsync(() => _model.Create(), true);
-
-            //    TrgCommitter(trg, taskEvent);
-            //}
 
             Debug.Print("Command executed");
         });
 
-        private async Task RunTransaction(int id, int offset, int sleepTime)
+        public ICommand Commit => new RelayCommand(c =>
         {
-            Debug.Print($"task {id} started");
+            Reference reference = _uiDoc.Selection.PickObject(ObjectType.Element, "Select MEPCurve");
+            MEPCurve mEPCurve = _doc.GetElement(reference) as MEPCurve;
+            RevitTask.RunAsync(() => RunDeleteTransaction(mEPCurve));
+            Debug.Print("Command executed");
+        });
 
-            using (var trg = new TransactionGroup(_doc, $"1"))
+        private void RunDeleteTransaction(MEPCurve mEPCurve)
+        {
+
+            using (var trg = new TransactionGroup(_doc, $"delete elem"))
             {
-                await RevitTask.RunAsync(() =>
+                trg.Start();
+
+                using (var tr = new Transaction(_doc, "del"))
                 {
-                    trg.Start();
-
-                    Debug.Print($"transaction {id} started");
-
-                    _model.Create(offset);
+                    tr.Start();
+                    _doc.Delete(mEPCurve.Id);
+                    tr.Commit();
                     _uiDoc.RefreshActiveView();
+                }
 
-                    Debug.Print($"Thread {id} sleeped.");
-                    Thread.Sleep(sleepTime);
-                    Debug.Print($"Thread {id} waked.");
+                Debug.Print($"{mEPCurve.IsValidObject}");
 
-                    Debug.Print($"transaction {id} executed");
-
-                    if (trg.HasStarted())
-                    {
-                        trg.RollBack();
-                    }
-                });
+                if (trg.HasStarted())
+                {
+                    trg.RollBack();
+                }
+                Debug.Print($"{mEPCurve.IsValidObject}");
             }
+
+            Debug.Print($"{mEPCurve.IsValidObject}");
         }
 
-        /// <summary>
-        /// Perform action to close transaction group.
-        /// </summary>
-        /// <param name="trg">Current opened transaction group.</param>
-        private void TrgCommitter(TransactionGroup trg, IWindowTaskEvent taskEvent)
-        {
-            if (trg.HasStarted() && !taskEvent.WindowClosed)
-            {
-                trg.RollBack();
-                //TaskDialog.Show($"{GetType().Name}", $"trg {_id} rolled");
-            }
-            else if (trg.HasStarted() && taskEvent.WindowClosed)
-            {
-                trg.Commit();
-                //TaskDialog.Show($"{GetType().Name}", $"trg {_id} committed");
-            }
-            else
-            {
-                TaskDialog.Show($"{GetType().Name}", "trg is not closed due to it hasn't been started.");
-            }
-        }
 
 
         public event EventHandler RollBackHandler;
@@ -155,6 +106,8 @@ namespace DS.RevitApp.TransactionTest.ViewModel
 
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<EventType> Event;
+
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             if (PropertyChanged != null)
