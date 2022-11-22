13af9740aa496a360794b4d82c183826b0da4b5b
Данил Сараев <leonardo147wot@gmail.com>
2022-11-22 23:38:11 +0300

    StartForm updated

---
+++ DS.RevitCommand.PostCommandTest/ExternalCommand.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace DS.RevitCommand.PostCommandTest
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExternalCommand : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+           ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            Autodesk.Revit.ApplicationServices.Application application = uiapp.Application;
+
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            StartWindow startWindow = new StartWindow(uiapp);
+            startWindow.Show();
+
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
