4294034c02b9ace546053e20d67c4dd2d495ebc0
Данил Сараев <leonardo147wot@gmail.com>
2023-05-11 18:02:49 +0300

    TransactioinTest updated

---
+++ DS.RevitApp.TransactionTest/Model/Class1.cs
@@ -0,0 +1,38 @@
+﻿using DS.ClassLib.VarUtils.Events;
+using DS.ClassLib.VarUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RevitApp.TransactionTest.Model
+{
+    internal class Class1 : IEvent<EventType>
+    {
+        public event EventHandler<EventType> Event;
+
+        public void Backward()
+        {
+            if (Event != null)
+            {
+                Event?.Invoke(this, EventType.Backward);
+            }
+        }
+
+        public void Apply()
+        {
+            Event?.Invoke(this, EventType.Apply);
+        }
+
+        public void RollBack()
+        {
+            Event?.Invoke(this, EventType.Rollback);
+        }
+
+        public void Close()
+        {
+            Event?.Invoke(this, EventType.Close);
+        }
+    }
+}
---
+++ DS.RevitCmd.MVVMTemplate3/StartWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace DS.RevitCmd.MVVMTemplate3
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow(TestViewModel testViewModel)
+        {
+            InitializeComponent();
+            this.DataContext = testViewModel;
+        }
+    }
+}
