786e15637d66ccdadad608a6631fa66979d9d15d
Leonardo2526 <leonardo147wot@gmail.com>
2023-07-27 15:24:00 +0300

    path refactored

--- DS.RevitApp.Test/AStarAlgorithmCDFTest.cs
+++ DS.RevitApp.Test/AStarAlgorithmCDFTest.cs
@@ -1,31 +1,23 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using DS.PathFinder;
-using DS.RevitLib.Utils.Connections.PointModels;
+using DS.ClassLib.VarUtils;
 using DS.RevitLib.Utils;
+using DS.RevitLib.Utils.Bases;
+using DS.RevitLib.Utils.Connections.PointModels;
+using DS.RevitLib.Utils.Creation.Transactions;
+using DS.RevitLib.Utils.Elements;
+using DS.RevitLib.Utils.Elements.MEPElements;
+using DS.RevitLib.Utils.Extensions;
+using DS.RevitLib.Utils.MEP;
+using DS.RevitLib.Utils.MEP.Creator;
+using DS.RevitLib.Utils.MEP.Models;
+using DS.RevitLib.Utils.ModelCurveUtils;
 using DS.RevitLib.Utils.PathCreators;
-using System;
+using DS.RevitLib.Utils.Solids.Models;
+using DS.RevitLib.Utils.Various;
+using DS.RevitLib.Utils.Various.Selections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DS.RevitLib.Utils.Various.Selections;
-using System.Configuration;
-using System.Xml.Linq;
-using DS.RevitLib.Utils.Various;
-using DS.RevitLib.Utils.Bases;
-using DS.RevitLib.Utils.Extensions;
-using DS.ClassLib.VarUtils.Points;
-using DS.RevitLib.Utils.Elements;
-using DS.RevitLib.Utils.Collisions.Detectors;
-using DS.RevitLib.Utils.Creation.Transactions;
-using DS.RevitLib.Utils.ModelCurveUtils;
-using DS.RevitLib.Utils.MEP.Creator;
-using DS.ClassLib.VarUtils;
-using DS.RevitLib.Utils.Elements.MEPElements;
-using DS.RevitLib.Utils.MEP.Models;
-using DS.RevitLib.Utils.MEP;
-using DS.RevitLib.Utils.Solids.Models;
 
 namespace DS.RevitApp.Test
 {
@@ -43,46 +35,56 @@ namespace DS.RevitApp.Test
             _trb = new ContextTransactionFactory(_doc);
             var path = Run();
 
-            if (path != null && path.Count != 0) 
-            { 
-                
+            if (path != null && path.Count != 0)
+            {
+
                 ShowLines(path);
-                ShowMEPCurves(path, _baseMEPCurve);
+                //ShowMEPCurves(path, _baseMEPCurve);
             }
         }
 
         private List<XYZ> Run()
         {
-            //var (startConnectionPoint, endConnectionPoint) = GetEdgePoints();
-            var (startConnectionPoint, endConnectionPoint) = GetPointsByConnectors();
+            var (startConnectionPoint, endConnectionPoint) = GetEdgePoints();
+            //var (startConnectionPoint, endConnectionPoint) = GetPointsByConnectors();
             if (startConnectionPoint is null || endConnectionPoint is null) { return null; }
 
             _baseMEPCurve = startConnectionPoint.Element as MEPCurve;
 
-            var step = 400.MMToFeet();
             var basisStrategy = new TwoMEPCurvesBasisStrategy(_uiDoc);
             var traceSettings = GetTraceSettings(_baseMEPCurve);
+            var planes = new List<PlaneType>()
+            {
+                //PlaneType.XY,
+                //PlaneType.XZ,
+                //PlaneType.YZ,
+            };
 
             var (docElements, linkElementsDict) = new ElementsExtractor(_doc).GetAll();
             var objectsToExclude = new List<Element>() { startConnectionPoint.Element };
-            if(startConnectionPoint.Element.Id != endConnectionPoint.Element.Id) 
-            { objectsToExclude.Add(endConnectionPoint.Element); }            
+            if (startConnectionPoint.Element.Id != endConnectionPoint.Element.Id)
+            { objectsToExclude.Add(endConnectionPoint.Element); }
+
+            var startMEPCurve = startConnectionPoint.Element as MEPCurve;
+            var endMEPCurve = endConnectionPoint.Element as MEPCurve;
 
             var pathFindFactory = new xYZPathFinder(_uiDoc, basisStrategy, traceSettings, docElements, linkElementsDict);
-            pathFindFactory.Build(step, objectsToExclude);
+            pathFindFactory.Build(startMEPCurve, endMEPCurve, objectsToExclude, true, planes);
 
             return pathFindFactory.FindPath(startConnectionPoint.Point, endConnectionPoint.Point);
         }
 
         private (ConnectionPoint startPoint, ConnectionPoint endPoint) GetEdgePoints()
         {
-            var selector = new PointSelector(_uiDoc) { AllowLink = false }; 
+            var selector = new PointSelector(_uiDoc) { AllowLink = false };
 
             var element = selector.Pick($"Укажите точку присоединения 1 на элементе.");
+            if(element == null) return (null, null);
             ConnectionPoint connectionPoint1 = new ConnectionPoint(element, selector.Point);
             if (connectionPoint1.IsValid)
             {
                 element = selector.Pick($"Укажите точку присоединения 2 на элементе.");
+                if (element == null) return (null, null);
                 ConnectionPoint connectionPoint2 = new ConnectionPoint(element, selector.Point);
                 return (connectionPoint1, connectionPoint2);
             }
@@ -106,7 +108,7 @@ namespace DS.RevitApp.Test
             ITraceSettings traceSettings = new TraceSettings()
             {
                 B = 100.MMToFeet(),
-                AList = new List<int>() { 30 }
+                AList = new List<int>() {30}
             };
             var solidModel = new SolidModel(mEPCurve.Solid());
             var mEPCurveModel = new MEPCurveModel(mEPCurve, solidModel);
@@ -123,7 +125,7 @@ namespace DS.RevitApp.Test
             {
                 var mcreator = new ModelCurveCreator(_doc);
                 for (int i = 0; i < path.Count - 1; i++)
-                {mcreator.Create(path[i], path[i + 1]); }
+                { mcreator.Create(path[i], path[i + 1]); }
             }, "ShowSolution");
         }
 
@@ -131,6 +133,7 @@ namespace DS.RevitApp.Test
         {
             var builder = new BuilderByPoints(baseMEPCurve, path);
             var mEPElements = builder.BuildSystem(new TransactionBuilder(_doc));
+            return;
             _trb.CreateAsync(() => _doc.Delete(baseMEPCurve.Id), "delete baseMEPCurve");
         }
     }
