0c04b45d44fab3f47c128493150102ec584f1f62
Leonardo2526 <leonardo147wot@gmail.com>
2023-08-05 13:35:08 +0300

    GetOutline added

--- DS.RevitApp.Test/AStarAlgorithmCDFTest.cs
+++ DS.RevitApp.Test/AStarAlgorithmCDFTest.cs
@@ -1,8 +1,10 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using DS.ClassLib.VarUtils;
+using DS.ClassLib.VarUtils.Points;
 using DS.RevitLib.Utils;
 using DS.RevitLib.Utils.Bases;
+using DS.RevitLib.Utils.Collisions;
 using DS.RevitLib.Utils.Connections.PointModels;
 using DS.RevitLib.Utils.Creation.Transactions;
 using DS.RevitLib.Utils.Elements;
@@ -16,8 +18,10 @@ using DS.RevitLib.Utils.PathCreators;
 using DS.RevitLib.Utils.Solids.Models;
 using DS.RevitLib.Utils.Various;
 using DS.RevitLib.Utils.Various.Selections;
+using DS.RevitLib.Utils.Visualisators;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace DS.RevitApp.Test
 {
@@ -27,6 +31,18 @@ namespace DS.RevitApp.Test
         private readonly Document _doc;
         private readonly ITransactionFactory _trb;
         private MEPCurve _baseMEPCurve;
+        List<BuiltInCategory> _exludedCathegories = new List<BuiltInCategory>()
+            {
+                BuiltInCategory.OST_DuctFittingInsulation,
+                BuiltInCategory.OST_DuctInsulations,
+                BuiltInCategory.OST_DuctCurvesInsulation,
+                BuiltInCategory.OST_PipeFittingInsulation,
+                BuiltInCategory.OST_PipeInsulations,
+                BuiltInCategory.OST_PipeCurvesInsulation,
+                BuiltInCategory.OST_TelephoneDevices,
+                BuiltInCategory.OST_Materials,
+                BuiltInCategory.OST_Rooms
+            };
 
         public AStarAlgorithmCDFTest(UIDocument uidoc)
         {
@@ -60,7 +76,23 @@ namespace DS.RevitApp.Test
                 //PlaneType.YZ,
             };
 
-            var (docElements, linkElementsDict) = new ElementsExtractor(_doc).GetAll();
+            var outline = GetOutline(startConnectionPoint.Point, endConnectionPoint.Point);
+            //var outline = GetOutline();
+            //outline.MinimumPoint.Show(_doc);
+            //_uiDoc.RefreshActiveView();
+            //outline.MaximumPoint.Show(_doc);
+            //_uiDoc.RefreshActiveView();
+
+            //var bb = new BoundingBoxXYZ();
+            //bb.Min = outline.MinimumPoint;
+            //bb.Max = outline.MaximumPoint;
+            //new TransactionBuilder(_doc).Build(() =>
+            //{
+            //    var visualizator = new BoundingBoxVisualisator(bb, _doc);
+            //    visualizator.Show();
+            //}, "show BoundingBox");
+
+            var (docElements, linkElementsDict) = new ElementsExtractor(_doc, _exludedCathegories, outline).GetAll();
             var objectsToExclude = new List<Element>() { startConnectionPoint.Element };
             if (startConnectionPoint.Element.Id != endConnectionPoint.Element.Id)
             { objectsToExclude.Add(endConnectionPoint.Element); }
@@ -69,7 +101,7 @@ namespace DS.RevitApp.Test
             var endMEPCurve = endConnectionPoint.Element as MEPCurve;
 
             var pathFindFactory = new xYZPathFinder(_uiDoc, basisStrategy, traceSettings, docElements, linkElementsDict);
-            pathFindFactory.Build(startMEPCurve, endMEPCurve, objectsToExclude, true, planes);
+            pathFindFactory.Build(startMEPCurve, endMEPCurve, objectsToExclude, outline, false, planes);
 
             return pathFindFactory.FindPath(startConnectionPoint.Point, endConnectionPoint.Point);
         }
@@ -107,8 +139,8 @@ namespace DS.RevitApp.Test
         {
             ITraceSettings traceSettings = new TraceSettings()
             {
-                B = 100.MMToFeet(),
-                AList = new List<int>() {30}
+                B = 20.MMToFeet(),
+                AList = new List<int>() {90}
             };
             var solidModel = new SolidModel(mEPCurve.Solid());
             var mEPCurveModel = new MEPCurveModel(mEPCurve, solidModel);
@@ -135,6 +167,47 @@ namespace DS.RevitApp.Test
             var mEPElements = builder.BuildSystem(new TransactionBuilder(_doc));
             return;
             _trb.CreateAsync(() => _doc.Delete(baseMEPCurve.Id), "delete baseMEPCurve");
+        }
+
+        private Outline GetOutline(XYZ startPoint = null, XYZ endPoint = null)
+        {
+            XYZ p1;
+            if(startPoint == null)
+            {
+                p1 = _uiDoc.Selection.PickPoint("Укажите первую точку зоны поиска.");
+                p1.Show(_doc);
+                _uiDoc.RefreshActiveView();
+            }
+            else
+            { p1 = startPoint; }
+
+            XYZ p2;
+            if (startPoint == null)
+            {
+                p2 = _uiDoc.Selection.PickPoint("Укажите вторую точку зоны поиска.");
+                p2.Show(_doc);
+                _uiDoc.RefreshActiveView();
+            }
+            else
+            { p2 = endPoint; }
+
+            var (minPoint, maxPoint) = XYZUtils.CreateMinMaxPoints(new List<XYZ>() { p1, p2 });
+
+            double offsetX = startPoint is null ? 0 : 5000.MMToFeet();
+            double offsetY = startPoint is null ? 0 : 5000.MMToFeet();
+            double offsetZ = 5000.MMToFeet();
+
+            var moveVector = new XYZ(XYZ.BasisX.X * offsetX, XYZ.BasisY.Y * offsetY, XYZ.BasisZ.Z * offsetZ);
+
+            var p11 = minPoint + moveVector;
+            var p12 = minPoint - moveVector;
+            (XYZ minP1, XYZ maxP1) = XYZUtils.CreateMinMaxPoints(new List<XYZ> { p11, p12 });
+
+            var p21 = maxPoint + moveVector;
+            var p22 = maxPoint - moveVector;
+            (XYZ minP2, XYZ maxP2) = XYZUtils.CreateMinMaxPoints(new List<XYZ> { p21, p22 });
+
+            return new Outline(minP1, maxP2);
         }
     }
 }
